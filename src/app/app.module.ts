da0e7e59d2146831ac434c74a8e04277049761f3
AndroGenium <chilachava.doc@gmail.com>
2024-06-06 17:37:37 +0400

    Navbar Layout*Non Functional Currently* Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
